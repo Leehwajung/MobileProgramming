18478275c7af06867a447f2763d5cc88d2776d13
이화중 <hj_tl22@live.co.kr>
2016-10-25 23:57:22 +0900

    Self 6-1

--- ReservationMaker161019/app/src/main/java/com/lhj/reservationmaker161019/MainActivity.java
+++ ReservationMaker161019/app/src/main/java/com/lhj/reservationmaker161019/MainActivity.java
@@ -5,23 +5,22 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private Chronometer crnTakenTime;
-    private Button btnStart, btnFinish;
-    private RadioButton rdbCal, rdbTime;
-    private CalendarView calReservation;
+    private RadioGroup rdgDateTime;
+    private DatePicker dpcReservation;
     private TimePicker tpcReservation;
-    private TextView textInitYear, textInitMonth, textInitDay, textInitHour, textInitMinute;
+    private TextView textYear, textMonth, textDay, textHour, textMinute;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,81 +29,83 @@ public class MainActivity extends AppCompatActivity {
 
         setTitle("시간 예약");
 
-        // 크로노미터
+        // 상단 크로노미터
         crnTakenTime = (Chronometer) findViewById(R.id.crnTakenTime);
 
-        // 버튼
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnFinish = (Button) findViewById(R.id.btnFinish);
+        // 날짜 및 시간 선택 라디오 버튼 그룹
+        rdgDateTime = (RadioGroup) findViewById(R.id.rdgDateTime);
+        RadioButton rdbDate = (RadioButton) findViewById(R.id.rdbDate);
+        RadioButton rdbTime = (RadioButton) findViewById(R.id.rdbTime);
 
-        // 라디오버튼 2개
-        rdbCal = (RadioButton) findViewById(R.id.rdbCal);
-        rdbTime = (RadioButton) findViewById(R.id.rdbTime);
-
-        // FrameLayout의 2개 위젯
-        calReservation = (CalendarView) findViewById(R.id.calReservation);
+        // 날짜 및 시간 피커
+        dpcReservation = (DatePicker) findViewById(R.id.dpcReservation);
         tpcReservation = (TimePicker) findViewById(R.id.tpcReservation);
 
-        // 텍스트 뷰 중에서 연, 월, 일, 시, 분 숫자
-        textInitYear = (TextView) findViewById(R.id.textInitYear);
-        textInitMonth = (TextView) findViewById(R.id.textInitMonth);
-        textInitDay = (TextView) findViewById(R.id.textInitDay);
-        textInitHour = (TextView) findViewById(R.id.textInitHour);
-        textInitMinute = (TextView) findViewById(R.id.textInitMinute);
-
-        // 처음에는 2개를 안보이게 설정
-        tpcReservation.setVisibility(View.INVISIBLE);
-        calReservation.setVisibility(View.INVISIBLE);
+        // 연, 월, 일, 시, 분 텍스트 뷰
+        textYear = (TextView) findViewById(R.id.textYear);
+        textMonth = (TextView) findViewById(R.id.textMonth);
+        textDay = (TextView) findViewById(R.id.textDay);
+        textHour = (TextView) findViewById(R.id.textHour);
+        textMinute = (TextView) findViewById(R.id.textMinute);
 
 
-        rdbCal.setOnClickListener(new View.OnClickListener() {
-            
-            @Override
-            public void onClick(View view) {
-                tpcReservation.setVisibility(View.INVISIBLE);
-                calReservation.setVisibility(View.VISIBLE);
-            }
-        });
-
-        rdbTime.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                tpcReservation.setVisibility(View.VISIBLE);
-                calReservation.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        // 타이머 설정
-        btnStart.setOnClickListener(new View.OnClickListener() {
+        // 크로노미터를 클릭하면 타이머 시작
+        crnTakenTime.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 crnTakenTime.setBase(SystemClock.elapsedRealtime());
                 crnTakenTime.start();
                 crnTakenTime.setTextColor(Color.RED);
+                rdgDateTime.setVisibility(View.VISIBLE);
             }
         });
 
-        // 버튼을 클릭하면 날짜,시간을 가져옴
-        btnFinish.setOnClickListener(new View.OnClickListener() {
+        // 날짜 선택 활성화
+        rdbDate.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
+                tpcReservation.setVisibility(View.INVISIBLE);
+                dpcReservation.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // 시간 선택 활성화
+        rdbTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                tpcReservation.setVisibility(View.VISIBLE);
+                dpcReservation.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // 연도를 롱클릭하면 타이머 중지
+        textYear.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View view) {
                 crnTakenTime.stop();
                 crnTakenTime.setTextColor(Color.BLUE);
 
-                java.util.Calendar curDate = java.util.Calendar.getInstance();
-                curDate.setTimeInMillis(calReservation.getDate());
+                textYear.setText(adaptForText(dpcReservation.getYear()));
+                textMonth.setText(adaptForText(1 + dpcReservation.getMonth()));
+                textDay.setText(adaptForText(dpcReservation.getDayOfMonth()));
 
-                textInitYear.setText(Integer.toString(curDate.get(Calendar.YEAR)));
-                textInitMonth.setText(Integer.toString(1 + curDate.get(Calendar.MONTH)));
-                textInitDay.setText(Integer.toString(curDate.get(Calendar.DATE)));
+                textHour.setText(adaptForText(tpcReservation.getHour()));
+                textMinute.setText(adaptForText(tpcReservation.getMinute()));
 
-                textInitHour.setText(Integer.toString(tpcReservation.getCurrentHour()));
-                textInitMinute.setText(Integer.toString(tpcReservation.getCurrentMinute()));
+                rdgDateTime.setVisibility(View.INVISIBLE);
+                tpcReservation.setVisibility(View.INVISIBLE);
+                dpcReservation.setVisibility(View.INVISIBLE);
 
+                return false;
             }
         });
+    }
+
+    private String adaptForText(int i) {
+        return String.format(Locale.getDefault(), "%d", i);
     }
 }
