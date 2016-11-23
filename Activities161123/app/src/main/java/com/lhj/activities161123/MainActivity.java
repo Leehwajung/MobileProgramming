d92b4bc5072484d0c5c94811ec65cd1938e25a75
이화중 <hj_tl22@live.co.kr>
2016-11-23 18:04:27 +0900

    Project 10-1

---
+++ Activities161123/app/src/main/java/com/lhj/activities161123/MainActivity.java
@@ -0,0 +1,25 @@
+package com.lhj.activities161123;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button btnNewAct = (Button) findViewById(R.id.btnNewAct);
+        btnNewAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Activities161123/app/src/main/java/com/lhj/activities161123/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.lhj.activities161123;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        final Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
